1f7ff2ec38f3e4b4102c4530b691a7edb29f97f9
SebastianK <167713089+FabianCochonPro@users.noreply.github.com>
2024-09-19 07:39:38 -0500

    Update Ejercicio 2.cpp

--- Ejercicio 2.cpp
+++ Ejercicio 2.cpp
@@ -3,22 +3,23 @@
 using namespace std;
 int fibonacci(int n)
 {
-  int e{1},u,tempU=0 ;
-  for(int i{0};i<=n;i++)
-  {
-    
-    cout<<e<<" ";
-    tempU=u;
-    u=e;
-    
-    e=e+tempU;
-    
-  }
-  cout<<endl;
-  return e;
+    int e{ 2 }, u{1 }, tempU = 0;
+    for (int i{ 0 }; i < n-1; i++)
+    {
+        if (i == 0)
+            cout << u <<" ";
+        cout << e << " ";
+        tempU = u;
+        u = e;
+
+        e = e + tempU;
+
+    }
+    cout << endl;
+    return e;
 }
 int main()
 {
-  fibonacci(10);
-  
+    fibonacci(10);
+
 }

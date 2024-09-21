f012f38a674ae3fcca749e9fec90f0ee8a35357d
SebastianK <167713089+FabianCochonPro@users.noreply.github.com>
2024-09-20 23:05:51 -0500

    Update Ejercicio 3.cpp

--- Ejercicio 3.cpp
+++ Ejercicio 3.cpp
@@ -1,43 +1,17 @@
 #include <iostream>
-
 using namespace std;
-void largestprimefactor(int m)
-{
-    int e;
-    for (int i=2; i<m; i++)
-    {
-        if(m%i==0)
-        {
-            bool P=true;
-            for(int j=2;j<i;j++)
-            {
-                if(i%j==0)
-                {   
-                    P=false;
-                    break;
-                 }    
-            }        
-            if(P)
-            {
-                 cout<<i<<" "; 
-                 e=i;
-            }
-                
-                
-                
-            
-        
-          
-        
-        }
-        
-    } 
-    
-
-    cout<<"El numero mas largo es :"<<e;
-}
 int main()
 {
-  
-  largestprimefactor(600851475143);
+   long long n=600851475143,i{3};int primoM{0};
+   while(i<=n)
+   {
+     primoM=i;
+     if(n%i==0)
+     {
+        n/=i;
+     }
+     i=i+2;
+   }
+   cout<<"El primo mayor "<<primoM;
+   return 0;
 }

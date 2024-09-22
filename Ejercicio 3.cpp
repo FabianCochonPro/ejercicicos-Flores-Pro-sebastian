fe6dfb5ea3541fb7472f7c89aa96a4480c2ff6d3
SebastianK <167713089+FabianCochonPro@users.noreply.github.com>
2024-09-22 00:03:56 -0500

    Update Ejercicio 3.cpp

--- Ejercicio 3.cpp
+++ Ejercicio 3.cpp
@@ -5,12 +5,25 @@ int main()
    long long n=600851475143,i{3};int primoM{0};
    while(i<=n)
    {
-     primoM=i;
+     
+     
      if(n%i==0)
      {
         n/=i;
+        for(int j=2;j<i;j++)
+        {
+            if(i%j==0)
+            {
+
+                break;
+            }
+            else
+                primoM=i;
+        }
+    
      }
      i=i+2;
+    
    }
    cout<<"El primo mayor "<<primoM;
    return 0;
